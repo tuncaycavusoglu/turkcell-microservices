361bf35d91b78c4c9bd8fdb08420b24b3c22302d
Koray Guney <koray.guney@hotmail.com>
2022-10-25 20:48:02 +0300

    Initial commit

---
+++ customer-service/src/main/java/com/turkcell/customerservice/model/Customer.java
@@ -0,0 +1,12 @@
+package com.turkcell.customerservice.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+    private String firstname;
+    private String lastname;
+    private String email;
+}
